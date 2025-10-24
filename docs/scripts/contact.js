d838cd2a87b3b9cb07faacbcdc5e9b47b53df701
Lap Shun Yu <yulapshun@gmail.com>
2025-10-24 22:07:25 +0200

    Add contact form

---
+++ docs/scripts/contact.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+  const form =  document.querySelector('#contactForm');
+  const formStatusContainer = document.querySelector('.form-submit-status');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    fetch(event.target.action, {
+      method: 'POST',
+      body: new FormData(event.target)
+    }).then(response => {
+      if (!response.ok) {
+        formStatusContainer.classList.add('error');
+        formStatusContainer.innerHTML = 'Something went wrong. Please try againg later.';
+        return;
+      }
+
+      form.querySelectorAll('input, textarea').forEach(a => a.value = '');
+      formStatusContainer.classList.remove('error');
+      formStatusContainer.innerHTML = 'Thank you for your message! We will get back to you soon.';
+    })
+  });
+}
