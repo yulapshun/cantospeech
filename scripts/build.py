1e9b62c9b2edbbcfdc02d2e1f8eed093c3dc2674
Lap Shun Yu <yulapshun@gmail.com>
2026-04-19 10:33:24 +0200

    Add blog page and sample post

--- scripts/build.py
+++ scripts/build.py
@@ -6,18 +6,21 @@
 # ///
 
 import os
+import re
 import gettext
 from os.path import isdir
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 LOCALES_DIR = os.path.join(os.path.dirname(__file__),"../src/locales")
 TEMPLATE_DIRS = [os.path.join(os.path.dirname(__file__), dir)
-                 for dir in ("../src/templates", "../src/templates/components")]
+                 for dir in ("../src/templates", "../src/templates/components", "../src/blog")]
 TEMPLATE_LIST = ["index.html", "about.html", "contact.html", "resources.html", "speech_therapy.html", "cantonese.html",
-                 "lidcombe.html", "fluency.html"]
+                 "lidcombe.html", "fluency.html", "blog.html"]
 OUTPUT_DIR = os.path.join(os.path.dirname(__file__), "../docs")
 LANGUAGES = ["en", "zh"]
 DEFAULT_LANGUAGE = "en"
+BLOG_DIR = os.path.join(os.path.dirname(__file__),"../src/blog")
+BLOG_OUTPUT_DIR_NAME = "blog"
 
 gettext.bindtextdomain("cantospeech", LOCALES_DIR)
 gettext.textdomain("cantospeech")
@@ -42,3 +45,23 @@ for template in TEMPLATE_LIST:
         file_path = os.path.join(OUTPUT_DIR, language, template)
         with open(file_path, "a", encoding="utf-8") as file:
             file.write(envs[language].get_template(template).render())
+
+for language in LANGUAGES:
+    if not os.path.isdir(os.path.join(OUTPUT_DIR, language, BLOG_OUTPUT_DIR_NAME)):
+        os.mkdir(os.path.join(OUTPUT_DIR, language, BLOG_OUTPUT_DIR_NAME))
+
+for template in os.listdir(BLOG_DIR):
+    input_file_path = os.path.join(BLOG_DIR, template)
+    for language in LANGUAGES:
+        output_file_path = os.path.join(OUTPUT_DIR, language, BLOG_OUTPUT_DIR_NAME, template)
+
+        if not os.path.isfile(input_file_path):
+            continue
+
+        with open(input_file_path, "r", encoding="utf-8") as input_file:
+            with open(output_file_path, "a", encoding="utf-8") as output_file:
+                match = re.search("<html lang=\"(.*)\">", input_file.read())
+                if match is not None and len(match.groups()) >= 1:
+                    lang = match.groups()[0]
+                    lang = lang.split('-')[0]
+                    output_file.write(envs[lang].get_template(template).render())
